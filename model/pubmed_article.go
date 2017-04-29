3ac5d107c4e0b17b6fb9839d8fa861a75fd6b6f0
jl987-Jie <jl987@cornell.edu>
2017-04-29 00:33:36 -0700

    added DateCompleted, DateCreated, DateRevised and Article fields to PubMedArticle model

--- model/pubmed_article.go
+++ model/pubmed_article.go
@@ -7,13 +7,32 @@ type PubmedArticleSet struct {
 // https://www.nlm.nih.gov/bsd/licensee/elements_descriptions.html
 type PubmedArticle struct {
 	MedlineCitation struct {
-		Owner string `xml:"Owner,attr"`
-		Status string `xml:"Status,attr"`
-		VersionDate string `xml:"VersionDate,attr"`
-		VersionID int `xml:"VersionID,attr"`
+		// Owner string `xml:"Owner,attr"`
+		// Status string `xml:"Status,attr"`
+		// VersionDate string `xml:"VersionDate,attr"`
+		// VersionID int `xml:"VersionID,attr"`
 		PMID struct {
 			Value int `xml:",chardata"`
-			Version int `xml:"Version,attr"`
+			// Version int `xml:"Version,attr"`
 		} `xml:"PMID"`
+		DateCompleted struct {
+			Year string `xml:"Year"`
+			Month string `xml:"Month"`
+			Day string `xml:"Day"`
+		} `xml:"DateCompleted"`
+		DateCreated struct {
+			Year string `xml:"Year"`
+			Month string `xml:"Month"`
+			Day string `xml:"Day"`
+		} `xml:"DateCreated"`
+		DateRevised struct {
+			Year string `xml:"Year"`
+			Month string `xml:"Month"`
+			Day string `xml:"Day"`
+		} `xml:"DateRevised"`
+		Article struct {
+			Journal struct {
+			} `xml:"Journal"`
+		} `xml:"Article"`
 	} `xml:"MedlineCitation"`
 }
